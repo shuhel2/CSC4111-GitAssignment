7fe017693bc0ff2a18fc321c3e1ba69e75a19834
Shuhel Ahmed <gh7270@wayne.edu>
2021-01-25 18:39:27 -0500

    Shuhel Ahmed

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-//Author: Amiangshu Bosu <amiangshu.bosu@wayne.edu>
+//Author: Shuhel Ahmed <gh7270@wayne.edu>
   
 // Header file for input output functions 
 #include<iostream>  
